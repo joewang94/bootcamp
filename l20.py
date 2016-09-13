9d7475c52e7505e4486996632927e7961fd09e40
Jingzhou Wang <jingzhou@caltech.edu>
2016-09-13 11:43:03 -0700

    Convert an array of cross-sectional areas  to diameters with commensurate units.

---
+++ l20.py
@@ -0,0 +1,17 @@
+import numpy as np
+def xa_to_diameter(xa):
+      """
+      Convert an array of cross-sectional areas
+        to diameters with commensurate units.
+      """
+
+      # Compute diameter from area
+      diameter = np.sqrt(xa *4 / np.pi)
+
+      return diameter
+A = np.array([[6.7, 1.3, 0.6, 0.7],
+              [0.1, 5.5, 0.4, 2.4],
+              [1.1, 0.8, 4.5, 1.7],
+              [0.0, 1.5, 3.4, 7.5]])
+
+b = np.array([1.1, 2.3, 3.3, 3.9])
